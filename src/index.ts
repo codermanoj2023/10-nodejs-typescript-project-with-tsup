9c60f095d57d8aab1e86283f3bf72923c6075291
Manoj Singh Rawat <natkhatkanharawatji@gmail.com>
2025-06-05 08:03:15 +0530

    production ready changes

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import express, {Express, Request, Response} from 'express';
 
 const app: Express = express();
 
+const port = process.env.PORT || 3000;
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, world!');
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
